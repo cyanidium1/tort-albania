a2fb592cffe8d863582229f195b818abfcba3e84
Fedir <99473719+cyanidium1@users.noreply.github.com>
2024-01-14 19:35:06 +0100

    tw check

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p className='text-green-500'>
+          if you see it in green tailwind works
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
